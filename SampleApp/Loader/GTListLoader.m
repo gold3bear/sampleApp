6fab58b6e610a327b719bb263d958c12170e6bd2
Bear Xiong <xyongxin@gmail.com>
2022-04-22 01:02:34 +0800

    42.常用开源网络框架AFNetworking

--- SampleApp/Loader/GTListLoader.m
+++ SampleApp/Loader/GTListLoader.m
@@ -6,17 +6,26 @@
 //
 
 #import "GTListLoader.h"
+#import <AFNetworking.h>
 
 @implementation GTListLoader
 -(void)loadListData{
-    NSString *urlString = @"http://localhost:8080";
-    NSURL *listURL = [NSURL URLWithString:urlString];
-//    NSURLRequest *listRequest = [NSURLRequest requestWithURL:listURL];
-    NSURLSession *session = [NSURLSession sharedSession];
-//    __unused NSURLSessionDataTask *dataTask = [session dataTaskWithRequest:listRequest];
-    NSURLSessionDataTask *dataTask= [session dataTaskWithURL:listURL completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
-        NSLog(@"%@",urlString);
+    
+    [[AFHTTPSessionManager manager] GET:@"http://127.0.0.1:8080/hello" parameters:nil headers:nil progress:^(NSProgress * _Nonnull downloadProgress) {
+        NSLog(@"progress ...");
+    } success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+        NSLog(@"success");
+    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+        NSLog(@"failure");
     }];
-    [dataTask resume];
+//    NSString *urlString = @"http://localhost:8080";
+//    NSURL *listURL = [NSURL URLWithString:urlString];
+////    NSURLRequest *listRequest = [NSURLRequest requestWithURL:listURL];
+//    NSURLSession *session = [NSURLSession sharedSession];
+////    __unused NSURLSessionDataTask *dataTask = [session dataTaskWithRequest:listRequest];
+//    NSURLSessionDataTask *dataTask= [session dataTaskWithURL:listURL completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+//        NSLog(@"%@",urlString);
+//    }];
+//    [dataTask resume];
 }
 @end
