f788ba05fa802a1ead05a8f07f691a1b05fac94c
Bear Xiong <xyongxin@gmail.com>
2022-04-21 23:19:54 +0800

    40.使用系统网络框架加载数据

--- SampleApp/Loader/GTListLoader.m
+++ SampleApp/Loader/GTListLoader.m
@@ -9,9 +9,14 @@
 
 @implementation GTListLoader
 -(void)loadListData{
-    NSString *urlString = @"https://www.google.com/search?q=xcode+vim+site:developer.apple.com&sxsrf=APq-WBs3t06nnYtWEZQjffFbw4L8eZeyoA:1650541569542&sa=X&ved=2ahUKEwi738G_iqX3AhUJmFYBHRilBrsQrQIoBHoECBAQBQ&biw=1920&bih=973&dpr=1";
-    __unused NSURL *listURL = [NSURL URLWithString:urlString];
-    
-    NSLog(@"%@",urlString);
+    NSString *urlString = @"http://localhost:8080";
+    NSURL *listURL = [NSURL URLWithString:urlString];
+//    NSURLRequest *listRequest = [NSURLRequest requestWithURL:listURL];
+    NSURLSession *session = [NSURLSession sharedSession];
+//    __unused NSURLSessionDataTask *dataTask = [session dataTaskWithRequest:listRequest];
+    NSURLSessionDataTask *dataTask= [session dataTaskWithURL:listURL completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        NSLog(@"%@",urlString);
+    }];
+    [dataTask resume];
 }
 @end
