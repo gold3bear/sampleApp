c7bc52dac2f83d3613544aca27c1b913d1934d9b
Bear Xiong <xyongxin@gmail.com>
2022-05-01 23:47:21 +0800

    47fileHandle

--- SampleApp/NewsController/Model/GTListLoader.m
+++ SampleApp/NewsController/Model/GTListLoader.m
@@ -41,7 +41,28 @@
     [self _getSandBoxPath];
 }
 -(void)_getSandBoxPath{
-    NSArray *pathArray = NSSearchPathForDirectoriesInDomains(NSLibraryDirectory,NSUserDomainMask,YES);
-    NSLog(@"");
+    NSArray *pathArray = NSSearchPathForDirectoriesInDomains(NSCachesDirectory,NSUserDomainMask,YES);
+    NSString *cachePath = [pathArray firstObject];
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+    //创建文件夹
+    NSString *dataPath = [cachePath stringByAppendingPathComponent:@"GTData"];
+    NSError *createError;
+    [fileManager createDirectoryAtPath:dataPath withIntermediateDirectories:YES attributes:nil error:&createError];
+    
+    //创建文件
+    NSString *listDataPath = [dataPath stringByAppendingPathComponent:@"list"];
+    NSData *listData = [@"abc" dataUsingEncoding:NSUTF8StringEncoding];
+    [fileManager createFileAtPath:listDataPath contents:listData attributes:nil];
+    
+    //查询文件
+    BOOL fileExist = [fileManager fileExistsAtPath:listDataPath];
+    
+    NSFileHandle *fileHandler = [NSFileHandle fileHandleForUpdatingAtPath:listDataPath];
+    [fileHandler seekToEndOfFile];
+    [fileHandler writeData:[@"def" dataUsingEncoding:NSUTF8StringEncoding]];
+    [fileHandler synchronizeFile];
+    [fileHandler closeFile];
+    
+    NSLog(@"fileExist %d",fileExist);
 }
 @end
